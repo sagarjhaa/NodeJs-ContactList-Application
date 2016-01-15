a18500958f54435503e6e3b64a5a555655ff053c
Sagar Jha <sagar.jhaa@gmail.com>
2016-01-15 12:57:58 -0500

    Applied Initial Bootstrap and Angular js CDN

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var app = express();
 
-app.get('/',function(req,res){
 
-	res.send('Hello from the World')
+// Index Page with the message of Hello World.
+// app.get('/',function(req,res){
+// 	res.send('Hello from the World')
+// });
 
-});
+app.use(express.static(__dirname + "/public"));
 
-
+//Command to listen on port 3000
 app.listen(3000);
+
+//Message to confirm that server is up and running
 console.log('Serve is listening on 3000');
