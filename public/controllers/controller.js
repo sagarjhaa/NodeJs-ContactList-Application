937aff2ab68425073a5b85892dd41446c4594aba
Sagar Jha <sagar.jhaa@gmail.com>
2016-01-15 14:42:02 -0500

    Part 2 - Completed
    Little idea of server side and application side

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,25 +1,31 @@
-function AppCtrl($scope){
+function AppCtrl($scope,$http){
 	console.log("Hello World from Controller....")
 
-	person1  = {
-		name : "Sagar",
-		email : "sagar.jhaa@gmail.com",
-		number : "(111) 111-1111"
-	};
+	$http.get('/contactlist').success(function(response){
+		console.log("I got the data that I requested");   //prints in the browser
+		$scope.contactlist = response; 
+	});
 
-	person2 = {
-		name  : "Snigdha", 
-		email : "snigdha@kent.edu",
-		number: "(222) 222-2222" 
-	};
 
-	person3 = {
-		name : "Raghav",
-		email : "raghav@gmail.com",
-		number : "(330) 475-2172"
-	};
+	// person1  = {
+	// 	name : "Sagar",
+	// 	email : "sagar.jhaa@gmail.com",
+	// 	number : "(111) 111-1111"
+	// };
 
-	var contactlist = [person1,person2,person3];
+	// person2 = {
+	// 	name  : "Snigdha", 
+	// 	email : "snigdha@kent.edu",
+	// 	number: "(222) 222-2222" 
+	// };
 
-	$scope.contactlist = contactlist;
+	// person3 = {
+	// 	name : "Raghav",
+	// 	email : "raghav@gmail.com",
+	// 	number : "(330) 475-2172"
+	// };
+
+	// var contactlist = [person1,person2,person3];
+
+	// $scope.contactlist = contactlist;
 };
--- server.js
+++ server.js
@@ -9,6 +9,32 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist',function(req,res){
+	console.log("I received a get request")  //prints in the terminal
+
+	person1  = {
+		name : "Sagar",
+		email : "sagar.jhaa@gmail.com",
+		number : "(111) 111-1111"
+	};
+
+	person2 = {
+		name  : "Snigdha", 
+		email : "snigdha@kent.edu",
+		number: "(222) 222-2222" 
+	};
+
+	person3 = {
+		name : "Raghav",
+		email : "raghav@gmail.com",
+		number : "(330) 475-2172"
+	};
+
+	var contactlist = [person1,person2,person3];
+
+	res.json(contactlist)
+});
+
 //Command to listen on port 3000
 app.listen(3000);
 
